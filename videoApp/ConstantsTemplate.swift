5367a615b30ca04308fa3a378a0b3533865197e3
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-06-23 19:51:26 -0400

    Added constantstemplate.swift and modified gitignore file so it will stop uploading my api keys

---
+++ videoApp/ConstantsTemplate.swift
@@ -0,0 +1,18 @@
+//
+//  ConstantsTemplate.swift
+//  videoApp
+//
+//  Created by XIN on 6/23/20.
+//  Copyright © 2020 XIN. All rights reserved.
+//
+
+//uncomment code below and add your own api keys
+
+//import Foundation
+//
+//struct Constants {
+//    static var API_KEY = ""
+//    static var PLAYLIST_ID = ""
+//    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+//
+//}
